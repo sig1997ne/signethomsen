42d8a6bf1e1cebf9363012bff3c5bd05644c8e8d
sig1997ne <141187664+sig1997ne@users.noreply.github.com>
2023-10-17 20:39:09 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 //get data from form  
 $name = $_POST['name'];
-$email= $_POST['email'];
+$email= $_POST['emailaddress'];
 $message= $_POST['message'];
 $to = "sig97ne@gmail.com";
 $subject = "Mail From website";
